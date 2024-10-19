7ffc7e52ab9851ec486e1f809945407732a83197
Govind Kumar Singh <76465140+govindrkumar@users.noreply.github.com>
2024-10-19 12:38:41 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,11 @@
+#intro program
+#Govind Kumar Singh
+#19-10-2024
+
+a=input("Enter your name: ")
+b=int(input("Enter your age: "))
+c=int(input("Enter your favourite number: "))
+
+txt=f"Hello {a}, you are {b} years old and your favourite number is {c} !"
+print(txt)
+print("Enter length of your name is: ",len(a))
